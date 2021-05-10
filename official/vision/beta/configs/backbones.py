d9bcad04111a1d107158ea70ccf2c70e09097cd5
Shixin Luo <luoshixin@google.com>
2021-05-10 14:56:38 +0800

    move utility functions to nn_layers and fix bugs

--- official/vision/beta/configs/backbones.py
+++ official/vision/beta/configs/backbones.py
@@ -95,6 +95,13 @@ class RevNet(hyperparams.Config):
 
 
 @dataclasses.dataclass
+class MobileDet(hyperparams.Config):
+  """Mobiledet config."""
+  model_id: str = 'MobileDetCPU'
+  filter_size_scale: float = 1.0
+
+
+@dataclasses.dataclass
 class Backbone(hyperparams.OneOfConfig):
   """Configuration for backbones.
 
@@ -107,6 +114,7 @@ class Backbone(hyperparams.OneOfConfig):
     spinenet: spinenet backbone config.
     spinenet_mobile: mobile spinenet backbone config.
     mobilenet: mobilenet backbone config.
+    mobiledet: mobiledet backbone config.
   """
   type: Optional[str] = None
   resnet: ResNet = ResNet()
@@ -116,3 +124,4 @@ class Backbone(hyperparams.OneOfConfig):
   spinenet: SpineNet = SpineNet()
   spinenet_mobile: SpineNetMobile = SpineNetMobile()
   mobilenet: MobileNet = MobileNet()
+  mobiledet: MobileDet = MobileDet()
--- official/vision/beta/modeling/layers/nn_layers.py
+++ official/vision/beta/modeling/layers/nn_layers.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 """Contains common building blocks for neural networks."""
-import math
 from typing import Callable, Dict, List, Optional, Tuple, Union
 
 from absl import logging
@@ -22,6 +21,7 @@ import tensorflow as tf
 from official.modeling import tf_utils
 
 # Type annotations.
+
 States = Dict[str, tf.Tensor]
 Activation = Union[str, Callable]
 
@@ -57,7 +57,7 @@ def round_filters(filters: int,
                   divisor: int = 8,
                   min_depth: Optional[int] = None,
                   round_down_protect: bool = True,
-                  skip: bool = False):
+                  skip: bool = False) -> int:
   """Rounds number of filters based on width multiplier."""
   orig_f = filters
   if skip or not multiplier:
@@ -173,7 +173,9 @@ class SqueezeExcitation(tf.keras.layers.Layer):
 
   def build(self, input_shape):
     num_reduced_filters = make_divisible(
-        self._in_filters * self._se_ratio, divisor=self._divisible_by)
+        self._in_filters * self._se_ratio,
+        divisor=self._divisible_by,
+        round_down_protect=self._round_down_protect)
 
     self._se_reduce = tf.keras.layers.Conv2D(
         filters=num_reduced_filters,
